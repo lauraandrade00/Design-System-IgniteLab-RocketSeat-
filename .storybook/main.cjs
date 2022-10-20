bf99b6ab47ef857ba8a149417901b6d3b39eff2b
Laura Andrade <lauraandradecamp@gmail.com>
2022-10-20 14:54:45 -0300

    Update main.cjs

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -16,9 +16,10 @@ module.exports = {
   "features": {
     "storyStoreV7": true
   },
+
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/design-system-ignitelab-rocketseat/'
+      config.base = '/design-system-ignitelab-rocketseat/.'
     }
 
     return config
